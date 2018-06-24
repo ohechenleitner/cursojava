87fc62ca1570596b48e8c87346a9316656ad002e
DESKTOP-A6616AS\Usuario <ohechenleitner@gmail.com>
2018-06-24 16:30:19 -0400

    prueba commit

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo con Java");
+        System.out.println("Hola Mundo con Java 2");
         // TODO code application logic here
     }
     
